2f08c8b6ef2d7caa678140f3b80a3d90c8e69b33
Javakin <danielharaldson@yahoo.com>
2017-09-28 16:03:52 +0200

    så er der overskrevet en build fil

--- Man2 Rob project-build/CMakeFiles/RobMan2.dir/DependInfo.cmake
+++ Man2 Rob project-build/CMakeFiles/RobMan2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/student/workspace/RoVi/Man2 Rob project/src/Main.cpp" "/home/student/workspace/RoVi/Man2 Rob project-build/CMakeFiles/RobMan2.dir/src/Main.cpp.o"
+  "/home/student/Desktop/RobotVision/Man2 Rob project/src/Main.cpp" "/home/student/Desktop/RobotVision/Man2 Rob project-build/CMakeFiles/RobMan2.dir/src/Main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
