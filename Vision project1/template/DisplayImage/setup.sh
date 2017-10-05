9df3621ddae3a4f4ac7ac980deb9d9549b1aed48
Javakin <danielharaldson@yahoo.com>
2017-10-05 16:14:48 +0200

    pic1 project has been created

---
+++ Vision project1/template/DisplayImage/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+####################################
+#
+# This schript will auto assemble
+# a DisplayImage project
+#
+####################################
+mkdir build
+cd build
+cmake ..
+make
+
---
+++ Vision project1/template/DisplayImage/src/DisplayImage.cpp
@@ -0,0 +1,38 @@
+/*
+ * RoVi1
+ * Example application to load and display an image
+ */
+
+// v1.0-4-gdfe246a
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    // Parse command line arguments
+    cv::CommandLineParser parser(argc, argv,
+        "{help   |            | print this message}"
+        "{@image | ./book.jpg | image path}"
+    );
+
+    if (parser.has("help")) {
+        parser.printMessage();
+        return 0;
+    }
+
+    // Load image
+    std::string filename = parser.get<std::string>("@image");
+    cv::Mat img = cv::imread(filename);
+
+    if (img.empty()) {
+        std::cout << "Input image not found at '" << filename << "'\n";
+        return 1;
+    }
+
+    // Show the image
+    cv::imshow("Image", img);
+    cv::waitKey();
+
+    return 0;
+}
