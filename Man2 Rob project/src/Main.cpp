b23f9929f9891f640b9724ebf9a508a01f865d87
Javakin <danielharaldson@yahoo.com>
2017-10-07 14:59:27 +0200

    så har vi streamlinet/automatiseret kommunikationen mellem cpp filen og lua programmet

--- Man2 Rob project/src/Main.cpp
+++ Man2 Rob project/src/Main.cpp
@@ -3,6 +3,7 @@
 #include <rwlibs/pathplanners/rrt/RRTPlanner.hpp>
 #include <rwlibs/pathplanners/rrt/RRTQToQPlanner.hpp>
 #include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+#include <fstream>
 
 using namespace std;
 using namespace rw::common;
@@ -39,9 +40,11 @@ bool checkCollisions(Device::Ptr device, const State &state, const CollisionDete
 }
 
 int main(int argc, char** argv) {
-    const string wcFile = "../Kr16WorkCell/Scene.wc.xml";
+    const string wcFile = "../Kr16WallWorkCell/Scene.wc.xml";
     const string deviceName = "KukaKr16";
     cout << "Trying to use workcell " << wcFile << " and device " << deviceName << endl;
+
+
 
     WorkCell::Ptr wc = WorkCellLoader::Factory::load(wcFile);
     Device::Ptr device = wc->findDevice(deviceName);
@@ -92,9 +95,67 @@ int main(int argc, char** argv) {
         cout << "Notice: max time of " << MAXTIME << " seconds reached." << endl;
     }
 
-    for (QPath::iterator it = path.begin(); it < path.end(); it++) {
-        cout << *it << endl;
-    }
+    // print the path in the cout terminal
+    /*for (unsigned  int n = 0; n < path.size(); n++){
+        std::cout << "setQ({";
+        cout << path[n][0];
+        for(unsigned int i = 1; i<6; i++){
+           cout << ", " << path[n][i];
+        }
+        std::cout << "})" << endl;
+    }*/
+
+
+
+    // write the data toan executable Lua file
+    ofstream myfile;
+     myfile.open ("../LUA_code.txt");
+    if (myfile.is_open()){
+        // initial code for the LUA file
+        myfile << "wc = rws.getRobWorkStudio():getWorkCell()" << endl;
+        myfile << "state = wc:getDefaultState()" << endl;
+        myfile << "device = wc:findDevice(\"KukaKr16\")" << endl;
+        myfile << "gripper = wc:findFrame(\"Tool\");" << endl;
+        myfile << "bottle = wc:findFrame(\"Bottle\");" << endl;
+        myfile << "table = wc:findFrame(\"Table\");" << endl;
+
+        myfile << "function setQ(q)" << endl;
+        myfile << "qq = rw.Q(#q,q[1],q[2],q[3],q[4],q[5],q[6])" << endl;
+        myfile << "device:setQ(qq,state)" << endl;
+        myfile << "rws.getRobWorkStudio():setState(state)" << endl;
+        myfile << "rw.sleep(0.1)" << endl;
+        myfile << "end" << endl;
+
+        myfile << "function attach(obj, tool)" << endl;
+        myfile << "rw.gripFrame(obj, tool, state)" << endl;
+        myfile << "rws.getRobWorkStudio():setState(state)" << endl;
+        myfile << "rw.sleep(0.1)" << endl;
+        myfile << "end" << endl;
+
+
+        myfile << "setQ({-3.142, -0.827, -3.002, -3.143, 0.099, -1.573})" << endl;
+        myfile << "attach(bottle,gripper)" << endl;
+
+
+
+         // insert RRT-path
+         for (unsigned  int n = 1; n < path.size(); n++){
+              myfile << "setQ({";
+              myfile << path[n][0];
+              for(unsigned int i = 1; i<6; i++){
+                 myfile << ", " << path[n][i];
+              }
+              myfile << "})" << endl;
+          }
+
+          myfile <<"attach(bottle,table)" << endl;
+
+
+          myfile.close();
+      }
+      else
+          return 1;
+
 
     cout << "Program done." << endl;
     return 0;
