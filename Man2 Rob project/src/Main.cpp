45833cb3f1823d9b7ee048070f0f8e7926d54c4e
waarbubble <kasper.lorenzen@gmail.com>
2017-10-10 11:20:42 +0200

    RobMan update

--- Man2 Rob project/src/Main.cpp
+++ Man2 Rob project/src/Main.cpp
@@ -39,75 +39,7 @@ bool checkCollisions(Device::Ptr device, const State &state, const CollisionDete
     return true;
 }
 
-int main(int argc, char** argv) {
-    const string wcFile = "../Kr16WallWorkCell/Scene.wc.xml";
-    const string deviceName = "KukaKr16";
-    cout << "Trying to use workcell " << wcFile << " and device " << deviceName << endl;
-
-    // initialize see
-    rw::math::Math::seed();
-
-    WorkCell::Ptr wc = WorkCellLoader::Factory::load(wcFile);
-    Device::Ptr device = wc->findDevice(deviceName);
-
-    // check that the device has been correctly added
-    if (device == NULL) {
-        cerr << "Device: " << deviceName << " not found!" << endl;
-        return 0;
-    }
-    const State state = wc->getDefaultState();
-
-    CollisionDetector detector(wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
-    PlannerConstraint constraint = PlannerConstraint::make(&detector,device,state);
-
-    /** Most easy way: uses default parameters based on given device
-        sampler: QSampler::makeUniform(device)
-        metric: PlannerUtil::normalizingInfinityMetric(device->getBounds())
-        extend: 0.05 */
-    //QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, device, RRTPlanner::RRTConnect);
-
-    /** More complex way: allows more detailed definition of parameters and methods */
-    QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(device),constraint.getQConstraintPtr());
-    QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
-    double extend = 0.1;
-    QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
-
-    //Q from(6,-0.2,-0.6,1.5,0.0,0.6,1.2);
-    //Q to(6,1.7,0.6,-0.8,0.3,0.7,-0.5); // Very difficult for planner
-    //Q to(6,1.4,-1.3,1.5,0.3,1.3,1.6);
-
-    Q from(6, -3.142, -0.827, -3.002, -3.143, 0.099, -1.573); // exercise path
-    Q to(6, 1.571, 0.006, 0.03, 0.153, 0.762, 4.49);
-
-
-    if (!checkCollisions(device, state, detector, from))
-        return 0;
-    if (!checkCollisions(device, state, detector, to))
-        return 0;
-
-    cout << "Planning from " << from << " to " << to << endl;
-    QPath path;
-    Timer t;
-    t.resetAndResume();
-    planner->query(from,to,path,MAXTIME);
-    t.pause();
-    cout << "Path of length " << path.size() << " found in " << t.getTime() << " seconds." << endl;
-    if (t.getTime() >= MAXTIME) {
-        cout << "Notice: max time of " << MAXTIME << " seconds reached." << endl;
-    }
-
-    // print the path in the cout terminal
-    /*for (unsigned  int n = 0; n < path.size(); n++){
-        std::cout << "setQ({";
-        cout << path[n][0];
-        for(unsigned int i = 1; i<6; i++){
-           cout << ", " << path[n][i];
-        }
-        std::cout << "})" << endl;
-    }*/
-
-
-
+void exportLUA(QPath path){
     // write the data toan executable Lua file
     ofstream myfile;
      myfile.open ("../LUA_code.txt");
@@ -154,10 +86,83 @@ int main(int argc, char** argv) {
 
           myfile.close();
       }
-      else
-          return 1;
+
+}
+
+int main(int argc, char** argv) {
+    const string wcFile = "../../Kr16WallWorkCell/Scene.wc.xml";
+    const string deviceName = "KukaKr16";
+    cout << "Trying to use workcell " << wcFile << " and device " << deviceName << endl;
+
+    // initialize see
+    rw::math::Math::seed();
+
+    WorkCell::Ptr wc = WorkCellLoader::Factory::load(wcFile);
+    Device::Ptr device = wc->findDevice(deviceName);
+
+    // check that the device has been correctly added
+    if (device == NULL) {
+        cerr << "Device: " << deviceName << " not found!" << endl;
+        return 0;
+    }
+
+    State state = wc->getDefaultState();
+    Q from(6, -3.142, -0.827, -3.002, -3.143, 0.099, -1.573); // exercise path
+    Q to(6, 1.571, 0.006, 0.03, 0.153, 0.762, 4.49);
+
+    device->setQ(from,state);
+    rw::kinematics::Frame*  gripper = wc->findFrame("Tool");
+    rw::kinematics::Frame*  bottle = wc->findFrame("Bottle");
+    rw::kinematics::Kinematics::gripFrame(gripper,device->getEnd(), state);
+    rw::kinematics::Kinematics::gripFrame(bottle,gripper, state);
+
+    CollisionDetector detector(wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
+    PlannerConstraint constraint = PlannerConstraint::make(&detector,device,state);
+
+    /** Most easy way: uses default parameters based on given device
+        sampler: QSampler::makeUniform(device)
+        metric: PlannerUtil::normalizingInfinityMetric(device->getBounds())
+        extend: 0.05 */
+    //QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, device, RRTPlanner::RRTConnect);
+
+    /** More complex way: allows more detailed definition of parameters and methods */
+    QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(device),constraint.getQConstraintPtr());
+    QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
+    double extend = 0.1;
+    QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
+    
+
+    if (!checkCollisions(device, state, detector, from))
+        return 0;
+    if (!checkCollisions(device, state, detector, to))
+        return 0;
+
+    cout << "Planning from " << from << " to " << to << endl;
+    QPath path;
+    Timer t;
+    t.resetAndResume();
+    planner->query(from,to,path,MAXTIME);
+    t.pause();
+    cout << "Path of length " << path.size() << " found in " << t.getTime() << " seconds." << endl;
+    if (t.getTime() >= MAXTIME) {
+        cout << "Notice: max time of " << MAXTIME << " seconds reached." << endl;
+    }
+
+    // print the path in the cout terminal
+    /*for (unsigned  int n = 0; n < path.size(); n++){
+        std::cout << "setQ({";
+        cout << path[n][0];
+        for(unsigned int i = 1; i<6; i++){
+           cout << ", " << path[n][i];
+        }
+        std::cout << "})" << endl;
+    }*/
+
+    exportLUA(path);
 
 
     cout << "Program done." << endl;
     return 0;
 }
+
+
